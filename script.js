d287d65ee6b8b022bdbc3210ce46694e61f3f9d9
djhenry1 <dhenry1@guilford.edu>
2024-04-11 10:09:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+window.addEventListener("scroll", function() {
+    // Get the current scroll position
+    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+    // Check if the scroll position is greater than a certain value (e.g., 500 pixels)
+    if (scrollPosition > 500) {
+        // If the scroll position is greater than 500 pixels, scroll to the top of the page
+        scrollToTop();
+    }
+});
+
+// Function to scroll to the top of the page
 function scrollToTop() {
+    // Scroll to the top of the page with smooth animation
     window.scrollTo({
         top: 0,
-        behavior: "smooth"});}
-
-        window.addEventListener("scroll", function() 
-        {var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-        if (scrollPosition > 500) {scrollToTop();}});function scrollToTop() 
-        {window.scrollTo({top: 0,behavior: "smooth"});}
+        behavior: "smooth"
+    });
+}
