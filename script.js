71013ab7ce25f3f63c716e7fdb07eae0bb718d32
djhenry1 <dhenry1@guilford.edu>
2024-04-11 09:49:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function scrollToTop() {
     window.scrollTo({
         top: 0,
         behavior: "smooth"});}
+
+window.addEventListener("scroll", function() {var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+if (scrollPosition > 500) {scrollToTop();}});
+        
