6ad537f5e9c038199dbaec15dded94c17b0f707b
djhenry1 <dhenry1@guilford.edu>
2024-04-11 09:57:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function scrollToTop() {
         top: 0,
         behavior: "smooth"});}
 
-window.addEventListener("scroll", function() {var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-if (scrollPosition > 500) {scrollToTop();}});
-        
+        window.addEventListener("scroll", function() 
+        {var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+        if (scrollPosition > 500) {scrollToTop();}});function scrollToTop() 
+        {window.scrollTo({top: 0,behavior: "smooth"});}
